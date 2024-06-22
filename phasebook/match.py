1cd25816762e12f9e55d22017597a03badb749cb
Carpel Sweet Dreams B. Lanto <carpelbalmes@gmail.com>
2024-06-22 13:30:23 +0800

    Improve the Matching Algorithm, Implement a Search Algorithm, Bonus Challenge done

--- phasebook/match.py
+++ phasebook/match.py
@@ -18,10 +18,6 @@ def match(match_id):
 
     return {"message": msg, "elapsedTime": end - start}, 200
 
-
 def is_match(fave_numbers_1, fave_numbers_2):
-    for number in fave_numbers_2:
-        if number not in fave_numbers_1:
-            return False
-
-    return True
+    fave_numbers_1_set = set(fave_numbers_1)
+    return all(number in fave_numbers_1_set for number in fave_numbers_2)
--- phasebook/search.py
+++ phasebook/search.py
@@ -24,7 +24,45 @@ def search_users(args):
     Returns:
         a list of users that match the search parameters
     """
+    id = args.get('id')
+    name = args.get('name')
+    age = args.get('age')
+    occupation = args.get('occupation')
 
-    # Implement search here!
+    results = []
+    sorted_results = []
 
-    return USERS
+    def add_user(user, matched_on):
+        if user not in results:
+            results.append((user, matched_on))
+
+    if id:
+        for user in USERS:
+            if user['id'] == id:
+                add_user(user, 'id')
+
+    if name:
+        name = name.lower()
+        for user in USERS:
+            if name in user['name'].lower():
+                add_user(user, 'name')
+
+    if age:
+        age = int(age)
+        for user in USERS:
+            if age - 1 <= user['age'] <= age + 1:
+                add_user(user, 'age')
+
+    if occupation:
+        occupation = occupation.lower()
+        for user in USERS:
+            if occupation in user['occupation'].lower():
+                add_user(user, 'occupation')
+
+    if not args:
+        return USERS
+    priority_order = {'id': 1, 'name': 2, 'age': 3, 'occupation': 4}
+    results.sort(key=lambda x: priority_order[x[1]])
+    sorted_results = [user[0] for user in results]
+
+    return sorted_results
